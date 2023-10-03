ca6ad42a2c2d4af92353bf18f463efb7964fc238
rjunbiju <rjunbiju@gmail.com>
2023-10-03 13:04:41 -0400

    Finished excercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let unreversed = string;
+let reversedstring = '';
+let i = unreversed.length-1;
+while (i >=0){
+    reversedstring += unreversed.charAt(i);
+    i--;
 };
-
+return reversedstring;
+}
 // Do not edit below this line
 module.exports = reverseString;
